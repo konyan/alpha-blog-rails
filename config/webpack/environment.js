037a8b8e16ded0d7e8b365ef3b2456542795cc62
konyan <devkonyan@gmail.com>
2020-02-28 04:53:56 +0630

    add home and setup custom css

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,13 @@
 const { environment } = require("@rails/webpacker");
 
-module.exports = environment;
-
 const webpack = require("webpack");
-
-environment.plugins.prepend(
+environment.plugins.append(
   "Provide",
   new webpack.ProvidePlugin({
-    $: "jquery/src/jquery",
-    jQuery: "jquery/src/jquery"
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"]
   })
 );
+
+module.exports = environment;
