b6397f93f290983a66ec84322f9245677075ae48
konyan <devkonyan@gmail.com>
2020-02-28 14:09:33 +0630

    add show view css and update styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "successfully created"
+      flash[:success] = "successfully created"
       redirect_to article_path(@article)
     else
       render :new
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully update"
+      flash[:success] = "Article was successfully update"
       redirect_to article_path(@article)
     else
       render :edit
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = 'Article was successfully deleted.'
+    flash[:danger] = 'Article was successfully deleted.'
     redirect_to articles_path
   end
 
