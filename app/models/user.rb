eb78b3f33936c045f677d87477c44bac6e7c0f7a
konyan <devkonyan@gmail.com>
2020-03-02 22:52:17 +0630

    add validation in user from username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true,
+            uniqueness:true,
+            length: {minimum: 3, maximum: 30}
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence:true,
+            uniqueness: true,
+            format: {with: VALID_EMAIL_REGEX}
 end
