8b70232274249d7475ffd43c61e84b84c0f9bd86
konyan <devkonyan@gmail.com>
2020-03-08 20:27:58 +0630

    change user static to user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "successfully created"
       redirect_to article_path(@article)
