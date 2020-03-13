929f9c64b211dde95145ac63b195810c3648e967
konyan <devkonyan@gmail.com>
2020-03-13 15:49:53 +0630

    add many to many relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, length: { maximum: 25 , minimum: 3}
   validates_uniqueness_of :name
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_193612) do
+ActiveRecord::Schema.define(version: 2020_03_13_084315) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
