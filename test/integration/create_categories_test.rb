f82d09d5a0b6626bd305caabc8e56e739311afc9
konyan <devkonyan@gmail.com>
2020-03-13 14:56:55 +0630

    add category ui and test create article

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = User.create(username: "Nyan", email: "n@gmail.com", password: "123456", admin: true)
+  end
+
   test "get new category form and create category" do
+    sign_in_as(@user)
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -14,6 +19,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test 'invalid category submission results in failure' do
+    sign_in_as(@user)
     get new_category_path
     assert_template 'categories/new'
     assert_select "h1", text: "Category New Category"
